1c66d3880603a4ac9b8e7800c8a55f631a0c2f96
roterrex <joel.roterex@gmail.com>
2024-07-24 16:56:34 +1000

    added some hints

--- src/main/java/frc/robot/subsystems/DifferentialDriveSub.java
+++ src/main/java/frc/robot/subsystems/DifferentialDriveSub.java
@@ -2,16 +2,19 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 /**
  * Drive Subsystem.
  * Handles all drive functionality.
  */
 public class DifferentialDriveSub extends SubsystemBase {
-  //TODO initialise the motors
+  //TODO initialise the motors (4 of them)
+  //private final WPI_TalonSRX name = new WPI_TalonSRX(id);
 
   public DifferentialDriveSub() {
     //TODO make two motors on each side run as a pair
+    //b.follow(a)
   }
 
   @Override
@@ -25,6 +28,7 @@ public class DifferentialDriveSub extends SubsystemBase {
    */
   public void arcade(double throttle, double steering) {
     // TODO take the throttle and steering and calc the power for each side (tank drive)
+    // a.set(speed -1->1)
   }
 
   /** Stops all motors. */
